16f60e83efbdf41c2c6cb3a45c40341d50f26ae5
MaxuzCL <96706766+Maxuz11@users.noreply.github.com>
2023-06-01 01:02:01 -0400

    arreglo de error en la descp en el header, notfoundPage perfect, avance de valoraciones

--- src/app/modules/Vecino/valoracion/valoracion.component.ts
+++ src/app/modules/Vecino/valoracion/valoracion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import * as CryptoJS from 'crypto-js';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-valoracion',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./valoracion.component.css']
 })
 export class ValoracionComponent implements OnInit {
+  solicitud!:FormGroup;  
+  data:any = sessionStorage.getItem('data');
+  
 
-  constructor() { }
-
+  constructor(private auth:AuthService,private fb:FormBuilder) { 
+  }
+  bytes:any = CryptoJS.AES.decrypt(this.data, this.auth.getKey()) ;
+  org:any  = this.bytes.toString(CryptoJS.enc.Utf8);
+  obj:any = JSON.parse(this.org);
+ 
+  id_junta:string = this.obj.id_junta_vec;
+  
+    //se la entregamos a las ocntantes que las va a usar este componente
   ngOnInit(): void {
+    this.solicitud = this.fb.group({
+      opinion: ["", Validators.required]
+    });
   }
 
+  stars: number[] = [1, 2, 3, 4, 5];
+  rating: number = 0;
+  hoverRating: number = 0;
+
+  rate(value: number) {
+    this.rating = value;
+  }
+  enter(starIndex: number) {
+    this.hoverRating = starIndex;
+  }
+  leave() {
+    this.hoverRating = 0;
+  }
+  submit(){
+    const a = this.solicitud.controls['opinion'].value;
+    const b = this.rating;
+
+    const c = [] = [a,b];
+  }
 }
